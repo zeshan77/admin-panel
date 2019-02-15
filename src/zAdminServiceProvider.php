063dcf52961afdd896ce46aee17058fc041f03c4
Zeshan Khattak <zeshan77@gmail.com>
2019-02-15 16:48:33 +0100

    removes routes.

--- src/zAdminServiceProvider.php
+++ src/zAdminServiceProvider.php
@@ -7,7 +7,6 @@ class zAdminServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/views', 'zadmin');
 
         $this->publishes([__DIR__ . '/views' => resource_path('views/vendor/zadmin')]);
