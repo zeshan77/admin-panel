6c96cc11722368a8cf221c0e0a664faa91e38f4c
Zeshan Khattak <zeshan77@gmail.com>
2019-02-18 22:13:49 +0100

    Making login functional.

--- src/views/forgot-password.blade.php
+++ src/views/forgot-password.blade.php
@@ -1,10 +1,22 @@
 @extends('zadmin::layouts.auth')
 
 @section('content')
-    <p class="mb-4 text-center">Reset password</p>
-    <input type="text" class="input" placeholder="Type your email">
-    <div class="btn">
-        <button class="btn-blue">Reset password</button>
-        <a class="a" href="/admin/login">Go back to login</a>
-    </div>
+    <form method="POST" action="{{ route('password.update') }}">
+        @csrf
+
+        <p class="mb-4 text-center">Reset password</p>
+        <div class="">
+            <input type="email" class="input" placeholder="Type your email" name="email" required autofocus>
+
+            @if ($errors->has('email'))
+                <span class="input-error" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+            @endif
+        </div>
+        <div class="btn">
+            <button class="btn-blue">Reset password</button>
+            <a class="a" href="/admin/login">Go back to login</a>
+        </div>
+    </form>
 @endsection
--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -1,11 +1,28 @@
 @extends('zadmin::layouts.auth')
 
 @section('content')
-    <p class="mb-4 text-center">Please fill in the following form to login.</p>
-    <input type="text" class="input" placeholder="Type your email">
-    <input type="password" class="input" placeholder="Type your password">
-    <div class="btn">
-        <button class="btn-blue">Login</button>
-        <a class="a" href="/admin/forgot-password">Forgot Password?</a>
-    </div>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+
+        <p class="mb-4 text-center">Please fill in the following form to login.</p>
+        <div class="mb-6">
+            <input type="email" class="input" placeholder="Type your email" name="email" value="{{ old('email') }}"
+                   required
+                   autofocus>
+            @if ($errors->has('email'))
+                <span class="input-error"><strong>{{ $errors->first('email') }}</strong></span>
+            @endif
+        </div>
+
+        <div class="mb-6">
+            <input type="password" name="password" class="input" placeholder="Type your password" required>
+            @if ($errors->has('password'))
+                <span class="input-error"><strong>{{ $errors->first('password') }}</strong></span>
+            @endif
+        </div>
+        <div class="btn">
+            <button class="btn-blue" type="submit">Login</button>
+            <a class="a" href="/admin/forgot-password">Forgot Password?</a>
+        </div>
+    </form>
 @endsection
